ccecc918ee04bcadff0c52d404df04c6679160c9
tomoten-umino <tomoten.umino@gmail.com>
2021-03-25 08:18:18 +0900

    update build.sh. commit docker-compose.yml for deploy

--- setup/build.sh
+++ setup/build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# import my-env file
+source ./.env
 # Program name
 PROGNAME=$(basename $0)
-
+# working directory
+NOW_DIR=`pwd`
+# deploy directory
+DEPLOY_DIR="../deploy"
 # option : docker image name
 IMAGE_NAME=""
 
@@ -53,7 +58,7 @@ function list() {
         exit 1
         ;;
       *)
-        if [ $prev_option != "-t" ]; then
+        if [ $prev_option != "-t" ] && [ $prev_option != "--tag" ]; then
           echo "$PROGNAME: illegal option or param -- '$(echo $1 | sed 's/^-*//')'" 1>&2
           usage
           exit 1
@@ -95,6 +100,23 @@ function main() {
   # build docker image
   echo "-- 4. build docker image -----------------------------------"
   sudo docker image build -t $IMAGE_NAME .
+
+  echo ""; echo ""
+  # write docker-compose.yml for deployment
+  echo "-- 5. write docker-compose.yml for deployment  -------------"
+  
+  export HOSTNAME_1=${HOSTNAME_1}
+  export HOSTNAME_2=${HOSTNAME_2}
+  export IP_ADDR_1=${IP_ADDR_1}
+  export IP_ADDR_2=${IP_ADDR_1}
+  export IMAGE_NAME=${IMAGE_NAME}
+  envsubst '$HOSTNAME_1' < docker-compose.yml.deploy |
+  envsubst '$HOSTNAME_2' |
+  envsubst '$IP_ADDR_1' |
+  envsubst '$IP_ADDR_2' |
+  envsubst '$IMAGE_NAME' > docker-compose.yml.deploy.tmp
+
+  mv docker-compose.yml.deploy.tmp ../deploy/docker-compose.yml  
 
   echo ""; echo ""
   echo "-- build.sh ran --------------------------------------------"
